e822a34ad0bafade9921ea0ae134f98474e5e2f9
PETE LAZURAN <pdlazuran@gmail.com>
2017-05-12 11:30:21 -0700

    create prototype for price. Needs testing. Project theme still undecided.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
+// back end -----------------------------------------------------
 function pizza(toppings, size) {
     this.toppings = toppings;
     this.pizzaSize = size;
+}
+
+pizza.prototype.price = function(){
+  var cost = 0;
+  if (newPizza.pizzaSize === "small"){
+    cost = 4;
+  }
+    else if (newPizza.pizzaSize === "med"){
+      cost = 5;
+    }
+      else if (newPizza.pizzaSize === "large"){
+        cost = 6;
+      }
+      for (var i =1; i < newPizza.toppings.length; i ++) {
+        cost += .10;
+      }
+      return cost
+
 }
 
 
@@ -12,7 +31,6 @@ function pizza(toppings, size) {
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
-      var pizzaPrice = 0;
       var toppings = [];
       $('input:checkbox[name=toppings]:checked').each(function(){
         toppings.push($(this).val())
